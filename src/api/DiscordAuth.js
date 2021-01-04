ec5b5b4472d7adad8b6964b75c3376d418159928
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-01-03 17:44:52 -0800

    init

---
+++ src/api/DiscordAuth.js
@@ -0,0 +1,52 @@
+const { Router } = require("express");
+const request = require("request-promise");
+
+const discord = Router();
+
+discord.get("/auth/discord", (req, res) => {
+  res.redirect(
+    "https://discord.com/api/oauth2/authorize" +
+      "?client_id=" +
+      process.env.CLIENT_ID +
+      "&redirect_uri=" +
+      encodeURIComponent("http://localhost:8080/api/v1/auth/discord/callback") +
+      "&response_type=code" +
+      "&scope=identify%20email"
+  );
+});
+
+discord.get("/auth/discord/callback", async (req, res) => {
+  let code = req.query.code;
+
+  let accessToken = await request({
+    method: "post",
+    url: "https://discord.com/api/v8/oauth2/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    form: {
+      code,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: "authorization_code",
+      redirect_uri: "http://localhost:8080/api/v1/auth/discord/callback",
+      scope: "identify email",
+    },
+    json: true,
+    simple: true,
+  });
+
+  let user = await request({
+    method: "get",
+    url: "https://discord.com/api/v8/users/@me",
+    headers: {
+      Authorization: "Bearer " + accessToken.access_token,
+    },
+    json: true,
+    simple: true,
+  });
+
+  res.send(user);
+});
+
+module.exports = discord;
---
+++ src/server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+
+const consola = require("consola");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const { PORT } = require("./utils/PORT.json");
+const { runInThisContext } = require("vm");
+
+class Server {
+  constructor() {
+    this.app = express();
+  }
+
+  start() {
+    this.setConfig();
+    this.setRequestLogger();
+    this.setRoutes();
+    this.setRateLimiter();
+
+    this.app.listen(PORT, () => {
+      consola.success(`Server started on port ${PORT}`);
+    });
+  }
+
+  setConfig() {
+    this.app.use(express.json());
+    this.app.use(cors());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+
+    require("dotenv").config();
+  }
+
+  setRequestLogger() {
+    this.app.use(async (req, res, next) => {
+      console.log(`[${req.method} - ${req.path}]`);
+
+      next();
+    });
+  }
+
+  setRateLimiter() {
+    const authLimiter = rateLimit({
+      windowMs: 60000,
+      max: 5,
+    });
+
+    this.app.use("/api/v1/auth/discord", authLimiter);
+  }
+
+  setRoutes() {
+    this.app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/frontend/index.html");
+    });
+
+    this.app.use(express.static(path.join(__dirname, "frontend")));
+
+    this.app.use("/api/v1", require("./api/DiscordAuth"));
+  }
+}
+
+module.exports = { Server };
