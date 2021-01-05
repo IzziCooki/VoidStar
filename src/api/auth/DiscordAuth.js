1dfa9d298913f16ba8cabc1c4cd62a78db9868b6
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-01-05 11:28:48 -0800

    fix removed file (DiscordAuth)

--- src/api/auth/DiscordAuth.js
+++ src/api/auth/DiscordAuth.js
@@ -1,78 +1,55 @@
-let express = require('express'),
-    app = express(),
-    passport = require('passport'),
-    session = require('express-session');
-let GithubStrategy = require('passport-github').Strategy;
+const { Router } = require("express");
+const request = require("request-promise");
+const { PORT } = require("../../utils/PORT.json");
 
-/***************************************************************
- *********** Github Configuration setup...
- ***************************************************************/
+const discord = Router();
 
-passport.use(new GithubStrategy({
-    clientID: "9c74caecec9d503e6453",
-    clientSecret: "4a3c4c676b8b6d8fb5abb37e8c357690ee368ff8",
-    callbackURL: "http://localhost:8000/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // we will just use the profile object returned by GitHub
-    return done(null, profile);
-  }
-));
-
-// Express and Passport Session
-app.use(session({secret: "randomletterthatiamtypingrightnow"}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-    // placeholder for custom user serialization
-    done(null, user);
+discord.get("/auth/discord", (req, res) => {
+  res.redirect(
+    "https://discord.com/api/oauth2/authorize" +
+      "?client_id=" +
+      process.env.CLIENT_ID +
+      "&redirect_uri=" +
+      encodeURIComponent(
+        `http://localhost:${PORT}/api/v1/auth/discord/callback`
+      ) +
+      "&response_type=code" +
+      "&scope=identify%20email"
+  );
 });
 
-passport.deserializeUser(function(user, done) {
-    // placeholder for custom user deserialization.
-    // maybe you are getoing to get the user from mongo by id?
+discord.get("/auth/discord/callback", async (req, res) => {
+  let code = req.query.code;
 
-    done(null, user); // null is for errors
-});
-
-// we will call this to start the GitHub Login process
-app.get('/auth/github', passport.authenticate('github'));
-
-// GitHub will call this URL
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/' }),
-  function(req, res) {
-      res.redirect('/');
+  let accessToken = await request({
+    method: "post",
+    url: "https://discord.com/api/v8/oauth2/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+    },
+    form: {
+      code,
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      grant_type: "authorization_code",
+      redirect_uri: `http://localhost:${PORT}/api/v1/auth/discord/callback`,
+      scope: "identify email",
+    },
+    json: true,
+    simple: true,
   });
 
-app.get('/logout', function (req, res) {
-    var html = "<ul>\
-      <li><a href='/auth/github'>GitHub</a></li>\
-      <li><a href='/logout'>logout</a></li>\
-    </ul>";
+  let user = await request({
+    method: "get",
+    url: "https://discord.com/api/v8/users/@me",
+    headers: {
+      Authorization: "Bearer " + accessToken.access_token,
+    },
+    json: true,
+    simple: true,
+  });
 
-    // data fetched from github server
-    if (req.isAuthenticated()) {
-      html += "<p>authenticated as user:</p>"
-      html += "<pre>" + JSON.stringify(req.user, null, 4) + "</pre>";
-    }
-
-    res.send(html);
+  res.redirect("/dashboard");
 });
 
-
-// Simple route middleware to ensure user is authenticated.
-//  Use this route middleware on any resource that needs to be protected.  If
-//  the request is authenticated (typically via a persistent login session),
-//  the request will proceed.  Otherwise, the user will be redirected to the login page.
-
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) { return next(); }
-    res.redirect('/dashboard')
-}
-
-
-app.listen(8000, function () {
-    console.log('App listening at port: 8000');
-});
+module.exports = discord;
