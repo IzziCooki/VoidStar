f58ced6ac0dd2ff20c3009c521df6ff39abec695
IzziCooki <63527400+IzziCooki@users.noreply.github.com>
2021-01-06 14:27:48 -0500

    Create VoidBot.py
    Added the start of the bot, at the moment it will respond to greetings and general stuff. Adding task functionality soon

---
+++ src/frontend/VoidBot.py
@@ -0,0 +1,23 @@
+import chatterbot
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Create object of ChatBot class with Logic Adapter
+bot = ChatBot(
+    'Buddy',  
+    logic_adapters=['chatterbot.logic.BestMatch','chatterbot.logic.TimeLogicAdapter'],
+)
+
+
+
+corpus_trainer = ChatterBotCorpusTrainer(bot)
+corpus_trainer.train("chatterbot.corpus.english")
+
+
+while (True):
+    user_input = input()
+    if (user_input == 'quit'):
+        break
+    response = bot.get_response(user_input)
+    print (response)
